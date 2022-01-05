46e5cd89f7b09fe20178bb1939d7bc573e40f0dd
deimagjas <deimagjas@gmail.com>
2022-01-04 22:36:40 -0500

    refactor node script

--- commands.sh
+++ commands.sh
@@ -7,26 +7,41 @@ function configure_network () {
                 VOLUME_IPC='cardano-node-ipc'
 		VOLUME_DATA='cardano-node-data'
 		VOLUME_KEYS='cardano-nft-keys'
-                LOCAL_PATH='/keys'
                 ;;
         testnet)
                 NETWORK='testnet'
                 VOLUME_IPC='cardano-testnet-node-ipc'
 		VOLUME_DATA='cardano-testnet-node-data'
 		VOLUME_KEYS='cardano-testnet-nft-keys'
-                LOCAL_PATH='/keys/testnet'
                 ;;
         *)
                 NETWORK=''
                 ;;
         esac
 }
-echo "Iniciando servicio de Docker..."
-sudo systemctl restart docker.service
-echo "Iniciando nodo Cardano en ${CHOOSE_NETWORK} ..."
-configure_network
-docker run -e NETWORK="${NETWORK}" -v "${VOLUME_IPC}":/ipc -v "${VOLUME_DATA}":/data -v  "${VOLUME_KEYS}":/keys inputoutput/cardano-node
-echo "Mostrando nombre del container"
-docker ps -a
-echo "Exportando CARDANO_NODE_SOCKET_PATH..."
-export CARDANO_NODE_SOCKET_PATH='/ipc/node.socket'
+function start_docker_service () {
+	sudo systemctl restart docker.service
+}
+function start_node () {
+	local arguments=(
+		-e NETWORK="${NETWORK}"
+		-v "${VOLUME_IPC}":/ipc 
+		-v "${VOLUME_DATA}":/data 
+		-v "${VOLUME_KEYS}":/keys
+		inputoutput/cardano-node
+	)
+	docker run "${arguments[@]}" 
+}
+function main () {
+	echo "Iniciando servicio de Docker..."
+	start_docker_service
+	echo "Iniciando nodo Cardano en ${CHOOSE_NETWORK} ..."
+	configure_network
+	start_node &
+	echo "Mostrando nombre del container"
+	docker ps -a
+	echo "Exportando CARDANO_NODE_SOCKET_PATH..."
+	export CARDANO_NODE_SOCKET_PATH='/ipc/node.socket'
+}
+echo "Cardano node"
+main
